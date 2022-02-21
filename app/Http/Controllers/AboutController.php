8a1d41eb9af8a71a7585e963364d8e345caa4428
= <=>
2022-02-21 11:29:45 +0700

    first commit

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return 'NIM : 2041720219 Nama : Rossa Akmalia';
+    }
+}
+
